8649b7d27430a721354be1b67c06fc091e7917e0
Jehanat <56012374+Jehanat@users.noreply.github.com>
2019-10-24 18:02:36 +0530

    Add files via upload

---
+++ AdvancedSpringbootData/src/main/java/com/jehanat/demo/AdvancedSpringbootDataApplication.java
@@ -0,0 +1,13 @@
+package com.jehanat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringbootDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSpringbootDataApplication.class, args);
+	}
+
+}
